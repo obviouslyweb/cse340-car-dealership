75b60597540cdb9cc82b914a17cdc9ad5858539c
obviouslyweb <conbutter12@gmail.com>
2026-03-05 15:07:06 -0700

    Added name var for testing.

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-// START: node server.js
-
-// Import express using ESM syntax
 import express from 'express';
 
-// Create an instance of an Express application
 const app = express();
 
-// Define a route handler for the root URL ('/')
+const name = process.env.NAME; // <-- NEW
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send(`Hello, ${name}!`); // <-- UPDATED
 });
 
-// Define the port number the server will listen on
 const PORT = 3000;
-
-// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
