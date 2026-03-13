2c36c6705893b849414c4dd8bf7abcda867bff4d
obviouslyweb <conbutter12@gmail.com>
2026-03-12 21:46:04 -0600

    Updated handling for getting vehicles on list.ejs

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 // Route handlers for static pages
-import { getFeaturedVehicles, getVehicles, getVehicleById } from '../models/vehicles.js';
+import { getFeaturedVehicles, getVehicles, getVehicleById, getCategories } from '../models/vehicles.js';
 
 const homePage = async (req, res) => {
     const featuredVehicles = await getFeaturedVehicles();
@@ -11,8 +11,28 @@ const aboutPage = (req, res) => {
 };
 
 const vehicleListPage = async (req, res) => {
-    const vehicles = await getVehicles();
-    res.render('vehicles/list', { title: 'Vehicles', vehicles });
+    const categories = await getCategories();
+
+    const categoryIdParam = parseInt(req.query.categoryId, 10);
+    const validCategoryIds = new Set(categories.map((cat) => cat.id));
+    const currentCategoryId = Number.isInteger(categoryIdParam) && validCategoryIds.has(categoryIdParam)
+        ? categoryIdParam
+        : null;
+
+    const showUnavailable = req.query.showUnavailable === '1';
+
+    const vehicles = await getVehicles({
+        categoryId: currentCategoryId || undefined,
+        includeUnavailable: showUnavailable
+    });
+
+    res.render('vehicles/list', {
+        title: 'Vehicles',
+        vehicles,
+        categories,
+        currentCategoryId,
+        showUnavailable
+    });
 };
 
 const vehicleDetailPage = async (req, res, next) => {
@@ -26,11 +46,5 @@ const vehicleDetailPage = async (req, res, next) => {
     }
     res.render('vehicles/detail', { title: `${vehicle.year} ${vehicle.make} ${vehicle.model}`, vehicle });
 };
-
-// const testErrorPage = (req, res, next) => {
-//     const err = new Error('This is a test error');
-//     err.status = 500;
-//     next(err);
-// };
 
 export { homePage, aboutPage, vehicleListPage, vehicleDetailPage };
